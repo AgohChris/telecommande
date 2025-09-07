a2c8f1d2ed34d6c92cc6367465ad12ca4abdaf05
AgohChris <agihchris90@gmail.com>
2025-09-07 11:21:20 +0000

    Auto-commit du : 07-09-2025 à 11:21:20

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import pyautogui
+import os
+
 
 app = Flask(__name__)
 
@@ -23,6 +25,7 @@ def stop():
     pyautogui.press("esc")     # Esc = quitter diaporama
     return "Diaporama arrêté"
 
+
 if __name__ == "__main__":
-    # On écoute sur toutes les interfaces (0.0.0.0)
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
